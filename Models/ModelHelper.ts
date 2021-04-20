a39589671a804d62383d39e949b3742b51088eb7
Sam Parton <sam@idevelopthings.com>
2021-04-20 18:33:11 +0100

    Update database options

--- Models/ModelHelper.ts
+++ Models/ModelHelper.ts
@@ -1,6 +1,10 @@
 import {MongoClient} from "mongodb";
 
-const database = new MongoClient(process.env.DB_URL);
+const database = new MongoClient(process.env.DB_URL, {
+	readPreference     : "primaryPreferred",
+	useNewUrlParser    : true,
+	useUnifiedTopology : true
+});
 
 export const collection = <T>(name: string) => {
 	return database.db(process.env.DB_NAME).collection<T>(name);
