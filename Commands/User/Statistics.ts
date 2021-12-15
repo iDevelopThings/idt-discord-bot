57b1ff9ef9a674e5feccf06287f27ee02134f284
Sam Parton <sam@idevelopthings.com>
2021-12-15 18:39:48 +0000

    Fox for NaN in stats embed

--- Commands/User/Statistics.ts
+++ Commands/User/Statistics.ts
@@ -30,27 +30,35 @@ export default class Statistics extends SlashCommand {
 
 		const member = guild().members.cache.get(userId);
 
+		const formatNumber = (val: number) => {
+			if(isNaN(val)) {
+				return 0;
+			}
+
+			return val;
+		}
+
 		return {
 			embeds : [
 				new MessageEmbed()
 					.setColor(member.displayHexColor)
 					.setAuthor(user.displayName, user.avatar)
-					.addField('Win/loss ratio', user.statistics.gambling.wins.count / user.statistics.gambling.losses.count, true)
+					.addField('Win/loss ratio', formatNumber(user.statistics.gambling.wins.count / user.statistics.gambling.losses.count), true)
 					.addField('Most invested', formatMoney(user.statistics.balance.mostInvested), true)
 					.addField('Most lost to taxes', formatMoney(user.statistics.balance.mostLostToTaxes), true)
-					.addField('Times gambled', user.statistics.gambling.totals.count, true)
+					.addField('Times gambled', formatNumber(user.statistics.gambling.totals.count), true)
 					.addField('Biggest win/loss amount', formatMoney(user.statistics.gambling.totals.mostMoney), true)
-					.addField('Messages sent', user.statistics.activity.messagesSent, true),
+					.addField('Messages sent', formatNumber(user.statistics.activity.messagesSent), true),
 				new MessageEmbed()
 					.setColor("GREEN")
 					.setTitle('Gambling Wins')
-					.addField('Wins', user.statistics.gambling.wins.count, true)
+					.addField('Wins', formatNumber(user.statistics.gambling.wins.count), true)
 					.addField('Biggest win', formatMoney(user.statistics.gambling.wins.mostMoney), true)
 					.addField('Total won', formatMoney(user.statistics.gambling.wins.totalMoney), true),
 				new MessageEmbed()
 					.setColor("RED")
 					.setTitle('Gambling Losses')
-					.addField('Losses', user.statistics.gambling.losses.count, true)
+					.addField('Losses', formatNumber(user.statistics.gambling.losses.count), true)
 					.addField('Biggest loss', formatMoney(user.statistics.gambling.losses.mostMoney), true)
 					.addField('Total lost', formatMoney(user.statistics.gambling.losses.totalMoney), true)
 
